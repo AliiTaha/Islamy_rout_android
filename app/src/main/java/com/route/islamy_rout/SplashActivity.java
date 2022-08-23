80f767dd415872dfe92eb2578ee7c670b329cac3
clinic <taha96735@gmail.com>
2022-08-23 22:57:22 +0200

    finish splash_screen change color to splash_screen remove the toolbar

---
+++ app/src/main/java/com/route/islamy_rout/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.route.islamy_rout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startHomeActivity();
+            }
+
+
+        }, 2000);
+
+    }
+
+    private void startHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
